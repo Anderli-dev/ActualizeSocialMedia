b368bcd30e4c50abc6972f45f1518b1610059c10
Ivan Suprun <ivan.suprun2015@gmail.com>
2023-07-05 14:30:01 +0300

    style(home page): changed main text and his style
    Text changed to ActualizerSocialMedia and style font size.

--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -3,7 +3,7 @@ import React from "react";
 export function Home(){
     return(
         <div>
-            <p>Hello world!</p>
+            <h1 style={{fontSize: "60px"}}>ActualizerSocialMedia.</h1>
         </div>
     )
 }
